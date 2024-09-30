bf3605df3e97dd5d85f135ef7111952d954a39fb
Happynewyea <90592329+Happynewyea@users.noreply.github.com>
2024-09-30 18:19:57 +0900

    Initial commit

---
+++ src/main/java/net/hoys/turtlemod/TurtleModClient.java
@@ -0,0 +1,11 @@
+package net.hoys.turtlemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TurtleModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
